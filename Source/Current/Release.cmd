2d82f4f83be30317dee621159737b55f48f842c9
Tony Wall <tony@tonywall.com>
2018-07-21 16:06:22 +0200

    Add GIT source control commands to release script and test build.

--- Source/Current/Release.cmd
+++ Source/Current/Release.cmd
@@ -21,15 +21,14 @@ if %errorlevel% neq 0 goto Error
 
 echo.
 echo Update source (and delete extra files)...
-rem * TODO: Replace TFS commands with GIT equivalent
-rem tf undo "%~dp0..\..\Build\v1" /recursive /noprompt
-rem if %errorlevel% gtr 1 goto Error
-rem tf scorch "%~dp0..\..\Build\v1" /recursive /diff /noprompt
-rem if %errorlevel% gtr 1 goto Error
-rem tf get "%~dp0" /recursive /noprompt
-rem if %errorlevel% gtr 1 goto Error
-rem tf scorch "%~dp0" /recursive /diff /noprompt
-rem if %errorlevel% gtr 1 goto Error
+git reset "%~dp0" -- "..\..\Build\v1"
+if %errorlevel% gtr 1 goto Error
+git clean -d -f -x "%~dp0" -- "..\..\Build\v1"
+if %errorlevel% gtr 1 goto Error
+git pull -f "%~dp0..\.."
+if %errorlevel% gtr 1 goto Error
+git clean -d -f -x "%~dp0"
+if %errorlevel% gtr 1 goto Error
 
 echo.
 echo Versioning...
@@ -47,13 +46,8 @@ if %errorlevel% neq 0 goto Error
 if not exist "%~dp0..\..\Build\v1" goto TargetClean
 echo.
 echo Delete old build directory so that old or renamed items are cleaned...
-rem * TODO: Replace TFS commands with GIT equivalent
-rem tf delete "%~dp0..\..\Build\v1" /recursive
-rem if %errorlevel% gtr 1 goto Error
 rmdir "%~dp0..\..\Build\v1" /s /q
 if %errorlevel% neq 0 goto Error
-rem tf checkin "%~dp0..\..\Build\v1" /recursive /noprompt
-rem if %errorlevel% gtr 1 goto Error
 :TargetClean
 
 echo.
@@ -66,7 +60,6 @@ if %errorlevel% gtr 7 goto Error
 
 echo.
 echo Adding any new files in build directory...
-rem * TODO: Replace TFS commands with GIT equivalent
 rem tf add "%~dp0..\..\Build\" /recursive /noprompt /noignore
 rem if %errorlevel% gtr 1 goto Error
 
